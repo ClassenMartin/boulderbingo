bbf042080c7a168ea520fab42533f067a8aa16ac
Martin Classen <classenmartin1@gmail.com>
2024-11-10 14:40:28 +0100

    updating variablesFile

--- src/Variable.js
+++ src/Variable.js
@@ -2,18 +2,27 @@ import React from "react";
 import { useState } from "react";
 
 function Variable({ content, title }) {
-    const [variableArray, setVariableArray] = useState(content || []);
+  const [variableArray, setVariableArray] = useState(content || []);
   return (
     <div className="mapped">
-        <h2>Sort of variable: {title}</h2>
+      <h2>Sort of variable: {title}</h2>
       {variableArray.map((variable, index) => (
         <div key={index} className="singleVariable">
-          <h4>{variable?.name}</h4>
+          <div>
+            <h4>{variable?.name}</h4>
+          </div>
           {variable?.rgb ? (
-            <div>color: {variable?.rgb}</div>
+            <div>
+              <b>color:</b> {variable?.rgb}
+            </div>
           ) : (
-            <div>pictogram: {variable?.image}</div>
+            <div>
+              <b>pictogram:</b> {variable?.image}
+            </div>
           )}
+          <div>
+            <b>Definition:</b> {variable?.description}
+          </div>
         </div>
       ))}
     </div>
--- src/variablesFile.js
+++ src/variablesFile.js
@@ -1,107 +1,375 @@
-
-
 export const HOLDS = [
-    { name: "crimps", image: "" },
-    { name: "jugs", image: "" },
-    { name: "slopers", image: "" },
-    { name: "pockets", image: "" },
-    { name: "volumes", image: "" },
-    { name: "crack", image: "" },
-    { name: "pinches", image: "" },
-  ];
+  {
+    name: "crimps",
+    image: "",
+    description:
+      "Small holds that require the climber to grip with their fingers bent at a sharp angle, often using just the fingertips.",
+  },
+  {
+    name: "jugs",
+    image: "",
+    description:
+      "Large, deep holds that can be easily gripped with an open hand, providing a comfortable rest for climbers.",
+  },
+  {
+    name: "slopers",
+    image: "",
+    description:
+      "Rounded, sloping holds that require the climber to use friction and body positioning to maintain grip.",
+  },
+  {
+    name: "pockets",
+    image: "",
+    description:
+      "Holds with a small hole or pocket, where the climber inserts one or more fingers to gain control.",
+  },
+  {
+    name: "volumes",
+    image: "",
+    description:
+      "Large, angular holds that extend from the wall, often used to create features like ledges or ramps.",
+  },
+  {
+    name: "crack",
+    image: "",
+    description:
+      "A hold formed by a narrow gap or crack in the rock, requiring a variety of techniques like jamming or stemming to climb.",
+  },
+  {
+    name: "pinches",
+    image: "",
+    description:
+      "Holds that require the climber to pinch with their fingers and thumb, often used for smaller or awkwardly shaped grips.",
+  },
+];
 
 export const COLORS = [
-  { name: "red", rgb: "rgb(255, 0, 0)" },
-  { name: "blue", rgb: "rgb(0, 0, 255)" },
-  { name: "green", rgb: "rgb(0, 255, 0)" },
-  { name: "yellow", rgb: "rgb(255, 255, 0)" },
-  { name: "orange", rgb: "rgb(255, 165, 0)" },
-  { name: "purple", rgb: "rgb(128, 0, 128)" },
-  { name: "pink", rgb: "rgb(255, 192, 203)" },
-  { name: "brown", rgb: "rgb(165, 42, 42)" },
-  { name: "magenta", rgb: "rgb(255, 0, 255)" },
-  { name: "black", rgb: "rgb(0, 0, 0)" },
+  {
+    name: "red",
+    rgb: "rgb(255, 0, 0)",
+    description:
+      "A bright color often associated with passion, energy, and danger.",
+  },
+  {
+    name: "blue",
+    rgb: "rgb(0, 0, 255)",
+    description:
+      "A cool color often associated with calmness, trust, and the sky.",
+  },
+  {
+    name: "green",
+    rgb: "rgb(0, 255, 0)",
+    description:
+      "A color often associated with nature, growth, and tranquility.",
+  },
+  {
+    name: "yellow",
+    rgb: "rgb(255, 255, 0)",
+    description:
+      "A bright, vibrant color often associated with happiness, optimism, and caution.",
+  },
+  {
+    name: "orange",
+    rgb: "rgb(255, 165, 0)",
+    description:
+      "A warm, energetic color often associated with enthusiasm, creativity, and warmth.",
+  },
+  {
+    name: "purple",
+    rgb: "rgb(128, 0, 128)",
+    description:
+      "A color often associated with royalty, luxury, and creativity.",
+  },
+  {
+    name: "pink",
+    rgb: "rgb(255, 192, 203)",
+    description:
+      "A soft, gentle color often associated with love, compassion, and femininity.",
+  },
+  {
+    name: "brown",
+    rgb: "rgb(165, 42, 42)",
+    description:
+      "A warm, earthy color often associated with stability, reliability, and nature.",
+  },
+  {
+    name: "magenta",
+    rgb: "rgb(255, 0, 255)",
+    description:
+      "A vibrant color created by mixing red and blue, often associated with creativity and uniqueness.",
+  },
+  {
+    name: "black",
+    rgb: "rgb(0, 0, 0)",
+    description:
+      "A color often associated with power, elegance, and the absence of light.",
+  },
 ];
 
 export const MOVES = [
-    { name: "dyno", image: "" },
-    { name: "flip", image: "" },
-    { name: "paddle", image: "" },
-    { name: "mantle", image: "" },
-    { name: "heelhook", image: "" },
-    { name: "toehook", image: "" },
-    { name: "run in", image: "" },
-    { name: "runner", image: "" }
-  ];
+  {
+    name: "dyno",
+    image: "",
+    description:
+      "A dynamic move where the climber jumps to reach a hold, often involving both hands and feet leaving the wall.",
+  },
+  {
+    name: "flip",
+    image: "",
+    description:
+      "A move where the climber rotates their body in the air, usually to adjust to a new position or reach a hold.",
+  },
+  {
+    name: "paddle",
+    image: "",
+    description:
+      "A move where the climber uses both hands to press against the holds on either side, typically used in wide or overhung positions.",
+  },
+  {
+    name: "mantle",
+    image: "",
+    description:
+      "A move where the climber pushes down on a hold with their hands while bringing their body up over the top of a ledge or lip.",
+  },
+  {
+    name: "heelhook",
+    image: "",
+    description:
+      "A move where the climber places their heel on a hold to gain leverage and pull themselves up, often used on overhangs.",
+  },
+  {
+    name: "toehook",
+    image: "",
+    description:
+      "A move where the climber hooks their toes around a hold, typically used to gain stability or control on steep terrain.",
+  },
+  {
+    name: "run in",
+    image: "",
+    description:
+      "A move where the climber approaches the wall and uses momentum to run into a series of holds, often used in bouldering problems.",
+  },
+  {
+    name: "runner",
+    image: "",
+    description:
+      "A move where the climber moves fluidly along a series of holds, typically transitioning from one set of grips to the next with minimal effort.",
+  },
+];
 
-  export const TERRAINS = [
-    { name: "slab -10", image: "" },
-    { name: "slab -5", image: "" },
-    { name: "vertical 0", image: "" },
-    { name: "overhang 10", image: "" },
-    { name: "overhang 20", image: "" },
-    { name: "overhang 30", image: "" },
-    { name: "overhang 40", image: "" },
-    { name: "roof 50", image: "" },
-    { name: "roof 60", image: "" },
-    { name: "roof 70", image: "" },
-    { name: "roof 80", image: "" }
-  ];
+export const TERRAINS = [
+  { name: "slab -10", image: "", description: "" },
+  { name: "slab -5", image: "", description: "" },
+  { name: "vertical 0", image: "", description: "" },
+  { name: "overhang 10", image: "", description: "" },
+  { name: "overhang 20", image: "", description: "" },
+  { name: "overhang 30", image: "", description: "" },
+  { name: "overhang 40", image: "", description: "" },
+  { name: "roof 50", image: "", description: "" },
+  { name: "roof 60", image: "", description: "" },
+  { name: "roof 70", image: "", description: "" },
+  { name: "roof 80", image: "", description: "" },
+];
 
-  export const AMOUNTOFMOVES = [
-    { name: "two", image: "" },
-    { name: "three", image: "" },
-    { name: "four", image: "" },
-    { name: "five", image: "" },
-    { name: "six", image: "" },
-    { name: "seven", image: "" },
-    { name: "eight", image: "" },
-    { name: "nine", image: "" },
-    { name: "ten", image: "" },
-    { name: "eleven", image: "" },
-    { name: "twelve", image: "" },
-  ];
-  
-  export const STYLES = [
-    { name: "tricky", image: "" },
-    { name: "power", image: "" },
-    { name: "delicate", image: "" },
-    { name: "chaos", image: "" },
-    { name: "straight forward", image: "" },
-    { name: "ballern", image: "" },
-    { name: "low percentage", image: "" },
-  ];
-  
-  export const TOPICS = [
-    { name: "handcuffed", image: "" },
-    { name: "hide and seek", image: "" },
-    { name: "surprise", image: "" },
-    { name: "fast slow slow", image: "" },
-    { name: "slow fast slow", image: "" },
-    { name: "slow slow fast", image: "" },
-    { name: "fast slow fast", image: "" },
-    { name: "slow", image: "" },
-    { name: "fast", image: "" },
-    { name: "fast slow", image: "" },
-    { name: "slow fast", image: "" },
-  ];
-  
-  export const FEELINGS = [
-    { name: "rage", image: "" },
-    { name: "happiness", image: "" },
-    { name: "joy", image: "" },
-    { name: "hate", image: "" },
-    { name: "madness", image: "" },
-    { name: "we will all die", image: "" },
-  ];
-  
-  export const STARTS = [
-    { name: "one hold", image: "" },
-    { name: "two holds", image: "" },
-    { name: "run in", image: "" },
-    { name: "jumpstart", image: "" },
-    { name: "standstart", image: "" },
-    { name: "sitstart", image: "" },
-  ];
+export const AMOUNTOFMOVES = [
+  { name: "two", image: "", description: "" },
+  { name: "three", image: "", description: "" },
+  { name: "four", image: "", description: "" },
+  { name: "five", image: "", description: "" },
+  { name: "six", image: "", description: "" },
+  { name: "seven", image: "", description: "" },
+  { name: "eight", image: "", description: "" },
+  { name: "nine", image: "", description: "" },
+  { name: "ten", image: "", description: "" },
+  { name: "eleven", image: "", description: "" },
+  { name: "twelve", image: "", description: "" },
+];
 
+export const STYLES = [
+  {
+    name: "tricky",
+    image: "",
+    description:
+      "A climbing style that requires precise footwork, balance, and technique to overcome difficult moves.",
+  },
+  {
+    name: "power",
+    image: "",
+    description:
+      "A climbing style that relies on strength, explosiveness, and physical endurance to tackle challenging moves.",
+  },
+  {
+    name: "delicate",
+    image: "",
+    description:
+      "A climbing style that emphasizes subtle movements, light touches, and fine control to maintain balance and precision.",
+  },
+  {
+    name: "chaos",
+    image: "",
+    description:
+      "A climbing style where the climber uses a mix of powerful and unpredictable movements, often improvising to make it through tough sections.",
+  },
+  {
+    name: "straight forward",
+    image: "",
+    description:
+      "A climbing style that involves a direct approach with minimal technical complexity, focusing on clear, simple moves.",
+  },
+  {
+    name: "ballern",
+    image: "",
+    description:
+      "A bold and confident climbing style that involves taking risks and committing to powerful, dynamic movements.",
+  },
+  {
+    name: "low percentage",
+    image: "",
+    description:
+      "A climbing style that involves attempting difficult moves with a lower likelihood of success, often relying on precision and timing.",
+  },
+];
 
+export const TOPICS = [
+  {
+    name: "handcuffed",
+    image: "",
+    description:
+      "A climbing scenario where the climber’s hands are restricted, often requiring creative movement and body positioning.",
+  },
+  {
+    name: "hide and seek",
+    image: "",
+    description:
+      "A style of climbing where the climber must find and use obscure or hidden holds to progress.",
+  },
+  {
+    name: "surprise",
+    image: "",
+    description:
+      "A climbing style where unexpected moves or sequences challenge the climber's adaptability and problem-solving skills.",
+  },
+  {
+    name: "fast slow slow",
+    image: "",
+    description:
+      "A technique involving a mix of rapid, controlled movements followed by slow, deliberate actions to maintain balance.",
+  },
+  {
+    name: "slow fast slow",
+    image: "",
+    description:
+      "A climbing pattern where the climber starts with a slow, cautious approach, accelerates for a quick move, and then slows down again for precision.",
+  },
+  {
+    name: "slow slow fast",
+    image: "",
+    description:
+      "A style that emphasizes slow, careful movements with a burst of speed for a challenging or dynamic move.",
+  },
+  {
+    name: "fast slow fast",
+    image: "",
+    description:
+      "A technique where the climber uses quick movements followed by a slow and precise action, ending with another fast motion to complete the move.",
+  },
+  {
+    name: "slow",
+    image: "",
+    description:
+      "A climbing style that emphasizes slow, deliberate movements to maintain control and balance, often used in delicate or technical situations.",
+  },
+  {
+    name: "fast",
+    image: "",
+    description:
+      "A dynamic style of climbing that relies on speed, aggression, and momentum to complete movements quickly.",
+  },
+  {
+    name: "fast slow",
+    image: "",
+    description:
+      "A style where the climber alternates between fast, explosive moves and slower, more controlled actions to adapt to the terrain.",
+  },
+  {
+    name: "slow fast",
+    image: "",
+    description:
+      "A climbing method where the climber takes slow, careful actions before making a fast, decisive move to progress.",
+  },
+];
+
+export const FEELINGS = [
+  {
+    name: "rage",
+    image: "",
+    description: "A strong feeling of intense anger or fury.",
+  },
+  {
+    name: "happiness",
+    image: "",
+    description:
+      "A state of contentment and joy, often associated with positive experiences.",
+  },
+  {
+    name: "joy",
+    image: "",
+    description: "A deep feeling of great pleasure and delight.",
+  },
+  {
+    name: "hate",
+    image: "",
+    description:
+      "A profound feeling of dislike or animosity toward someone or something.",
+  },
+  {
+    name: "madness",
+    image: "",
+    description:
+      "A state of mental instability or extreme irrational behavior.",
+  },
+  {
+    name: "we will all die",
+    image: "",
+    description: "A grim realization of the inevitable nature of mortality.",
+  },
+];
+
+export const STARTS = [
+  {
+    name: "one hold",
+    image: "",
+    description:
+      "A climbing start where the climber begins with only one hand or foot on a hold, requiring precise movement to establish balance.",
+  },
+  {
+    name: "two holds",
+    image: "",
+    description:
+      "A climbing start where the climber begins with both hands or feet on separate holds, often requiring quick coordination and control.",
+  },
+  {
+    name: "run in",
+    image: "",
+    description:
+      "A start where the climber approaches the wall and uses momentum to quickly engage the first hold, often with a dynamic movement.",
+  },
+  {
+    name: "jumpstart",
+    image: "",
+    description:
+      "A start where the climber jumps to reach the first hold, requiring explosive power and timing.",
+  },
+  {
+    name: "standstart",
+    image: "",
+    description:
+      "A climbing start where the climber begins by standing on the ground and placing their hands or feet on the first holds to initiate the climb.",
+  },
+  {
+    name: "sitstart",
+    image: "",
+    description:
+      "A start where the climber begins seated on the ground, typically with hands and feet on the first holds, requiring strong core engagement to stand up.",
+  },
+];
