c5f1bdedd6f984747fd1e903c2d6ed9c1ee4ada8
Martin Classen <classenmartin1@gmail.com>
2024-11-17 18:33:47 +0100

    fixing bug

--- src/Variable.js
+++ src/Variable.js
@@ -17,55 +17,58 @@ function Variable({ content, title }) {
 
       {isOpen && (
         <div className="pt-3">
-          {variableArray.map(
-            (variable, index) =>
-              Object.keys(variable).length === 5 ? (
-                <div
-                  key={index}
-                  className={`flex justify-between ${
-                    index % 2 === 0 ? "bg-gray-200" : ""
-                  }`}
-                >
-                  <div className="flex-grow w-[calc(25%)] p-2">
-                    <b>name:</b> {variable?.name}
-                  </div>
-
-                  <div className="flex-grow w-[calc(25%)] p-2">
-                    <div><image src={variable?.image}/></div>
-                  </div>
-
-                  <div className="flex-grow w-[calc(25%)] p-2">
-                    <b>Definition:</b> {variable?.description}
-                  </div>
-
-                  <div className="flex-grow w-[calc(25%)] p-2">
-                    <b>V scale:</b> {variable?.vScale}
-                  </div>
+          {variableArray.map((variable, index) =>
+            Object.keys(variable).length === 5 ? (
+              <div
+                key={index}
+                className={`flex justify-between ${
+                  index % 2 === 0 ? "bg-gray-200" : ""
+                }`}
+              >
+                <div className="flex-grow w-[calc(25%)] p-2">
+                  <b>name:</b> {variable?.name}
                 </div>
-              ) : (
-                <div
-                  key={index}
-                  className={`flex justify-between ${
-                    index % 2 === 0 ? "bg-gray-200" : ""
-                  }`}
-                >
-                  <div className="flex-grow w-[calc(33%)] p-2">
-                    <b>name:</b> {variable?.name}
-                  </div>
-                  {variable?.rgb ? (
-                    <div className="flex-grow w-[calc(33%)] p-2">
-                      <b>color:</b> {variable?.rgb}
-                    </div>
-                  ) : (
-                    <div className="flex-grow w-[calc(33%)] p-2">
-                      <div><img src={variable?.image} className="border-black border-2"/></div>
-                    </div>
-                  )}
-                  <div className="flex-grow w-[calc(33%)] p-2">
-                    <b>Definition:</b> {variable?.description}
-                  </div>
+
+                <div className="flex-grow w-[calc(25%)] p-2">
+                  <image className="w-1/2" src={variable?.image} alt="" />
                 </div>
-              )
+
+                <div className="flex-grow w-[calc(25%)] p-2">
+                  <b>Definition:</b> {variable?.description}
+                </div>
+
+                <div className="flex-grow w-[calc(25%)] p-2">
+                  <b>V scale:</b> {variable?.vScale}
+                </div>
+              </div>
+            ) : (
+              <div
+                key={index}
+                className={`flex justify-between ${
+                  index % 2 === 0 ? "bg-gray-200" : ""
+                }`}
+              >
+                <div className="flex-grow w-[calc(33%)] p-2">
+                  <b>name:</b> {variable?.name}
+                </div>
+                {variable?.rgb ? (
+                  <div className="flex-grow w-[calc(33%)] p-2">
+                    <b>color:</b> {variable?.rgb}
+                  </div>
+                ) : (
+                  <div className="flex-grow w-[calc(33%)] p-2">
+                    <img
+                      src={variable?.image}
+                      className="border-black border-2 w-1/2"
+                      alt=""
+                    />
+                  </div>
+                )}
+                <div className="flex-grow w-[calc(33%)] p-2">
+                  <b>Definition:</b> {variable?.description}
+                </div>
+              </div>
+            )
           )}
         </div>
       )}
